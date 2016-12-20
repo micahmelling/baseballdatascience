fefeabd48ccc0d6f78686868068919cf4efe3ffe
Micah Melling <micahmelling@users.noreply.github.com>
2016-12-19 19:13:34 -0500

    Update Bokeh Example.py

--- Bokeh Example.py
+++ Bokeh Example.py
@@ -1,55 +1,29 @@
-
-# coding: utf-8
-
-# In[40]:
-
 #Import libraries
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import numpy as np
 
-
-# In[41]:
-
 #Use requests library to get the URL
 r  = requests.get("http://www.baseball-reference.com/teams/tgl.cgi?team=KCR&t=b&year=2016")
 data = r.text
 
-
-# In[ ]:
-
 #Create beautiful soup object from the data
 soup = BeautifulSoup(data)
-
-
-# In[43]:
 
 #Extract column headers
 column_headers = [th.getText() for th in 
                   soup.findAll('tr', limit=2)[0].findAll('th')]
 
-
-# In[44]:
-
 #Define data rows
 data_rows = soup.findAll('tr') 
-
-
-# In[45]:
 
 #Get data from table
 team_data = [[td.getText() for td in data_rows[i].findAll('td')]
             for i in range(len(data_rows))]
 
-
-# In[46]:
-
 #Convert to data frame
 df = pd.DataFrame(team_data, columns = column_headers)
-
-
-# In[23]:
 
 #Bokeh imports
 from bokeh.io import output_notebook, show
@@ -58,9 +32,6 @@ from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure
 from bokeh.io import output_notebook
 output_notebook()
-
-
-# In[36]:
 
 #Develop hover plot for hits and runs
 source = ColumnDataSource(
