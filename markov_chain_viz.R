0be6e7e5db4078590a9af8be3f789995241aef01
Micah Melling <micahmelling@users.noreply.github.com>
2017-07-26 22:18:16 -0500

    markov chain viz

---
+++ markov_chain_viz.R
@@ -0,0 +1,38 @@
+setwd("C:/Users/Micah/Desktop/Baseball Data Science/Markov Chain")
+
+# Library imports
+library(ggplot2)
+library(ggpubr)
+
+# Read in data
+transitions <- read.csv('scherzer_transitions.csv')
+pitch_counts <- read.csv('pitches_in_counts.csv')
+
+# Transition Probabilities Plot
+ggdotchart(transitions, x = "Transition", y = "Probability", 
+           color = "Probability",
+           sorting = "descending",                       
+           rotate = TRUE,                                
+           dot.size = 2,                                 
+           y.text.col = TRUE,                            
+           ggtheme = theme_pubr()
+)+
+  theme_cleveland() 
+
+# Pitch Counts Plot
+ggdotchart(pitch_counts, x = "pitch", y = "pitch_percentage",
+           color = "count", 
+           palette = c("#FF0000", "#00FF00", "#0000FF",
+                       "#00FFFF", "#800080", "#FF7F50",
+                       "#FF8C00", "#008000", "#00FA9A",
+                       "#4169E1", "#00FFFF", "#FFD700"),
+
+           sorting = "ascending",                        
+           add = "segments",                             
+           ggtheme = theme_pubr()                        
+)
+
+
+
+
+
