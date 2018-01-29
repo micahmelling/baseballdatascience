15b9ebad5ee88dd56969be64fc389ce23240406e
Micah Melling <micahmelling@users.noreply.github.com>
2018-01-28 18:05:02 -0600

    Update and rename player_recommender.py to python_scripts/pitcher_similarity.py

--- python_scripts/pitcher_similarity.py
+++ python_scripts/pitcher_similarity.py
@@ -1,11 +1,12 @@
-## Libraries and set working directory
+# Citation: Programming Collective Intelligence by Toby Segaran
+# Import Libraries
 import os
-os.chdir('C:\\Users\\Micah\\Desktop\Baseball Data Science')
 
 import pandas as pd
 import pymysql.cursors
 from math import sqrt
 from sklearn.preprocessing import MinMaxScaler
+
 
 ## Connect to the database
 connection = pymysql.connect(host='localhost',
@@ -14,34 +15,37 @@ connection = pymysql.connect(host='localhost',
                              db='xxxxx',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
-                             
-pitchers_query = '''
-select
-concat(master.nameFirst, ' ', master.nameLast) as "Name",
-pitching.yearID as "Year",
-pitching.W as "Wins",
-pitching.L as "Losses",
-pitching.G as "Appearances",
-pitching.GS as "Games_Started",
-pitching.CG as "Complete_Games",
-pitching.SHO as "Shutouts",
-pitching.SV as "Saves",
-pitching.IPouts as "Outs_Recorded",
-pitching.H as "Hits_Surrendered",
-pitching.ER as "Earned_Runs",
-pitching.HR as "Home_Runs_Surrendered",
-pitching.BB as "Walks_Surrendered",
-pitching.SO as "Strikeouts",
-pitching.BAOpp as "Opponent_Batting_Average",
-pitching.ERA as "ERA",
-pitching.R as "Runs_Surrendered"
 
-from pitching
-inner join master on pitching.playerID = master.playerID
 
-where pitching.YearID >= 1900;'''
+def ingest_data():
+    pitchers_query = '''
+    select
+    concat(master.nameFirst, ' ', master.nameLast) as "Name",
+    pitching.yearID as "Year",
+    pitching.W as "Wins",
+    pitching.L as "Losses",
+    pitching.G as "Appearances",
+    pitching.GS as "Games_Started",
+    pitching.CG as "Complete_Games",
+    pitching.SHO as "Shutouts",
+    pitching.SV as "Saves",
+    pitching.IPouts as "Outs_Recorded",
+    pitching.H as "Hits_Surrendered",
+    pitching.ER as "Earned_Runs",
+    pitching.HR as "Home_Runs_Surrendered",
+    pitching.BB as "Walks_Surrendered",
+    pitching.SO as "Strikeouts",
+    pitching.BAOpp as "Opponent_Batting_Average",
+    pitching.ERA as "ERA",
+    pitching.R as "Runs_Surrendered"
 
-pitchers = pd.read_sql(pitchers_query, connection)
+    from pitching
+    inner join master on pitching.playerID = master.playerID
+
+    where pitching.YearID >= 1900;'''
+
+    pitchers = pd.read_sql(pitchers_query, connection)
+    return pitchers
 
 def clean_data(pitchers):
     pitchers = pitchers.dropna()    
@@ -97,8 +101,7 @@ def clean_data(pitchers):
                          
     return pitchers
     
-pitchers = clean_data(pitchers)  
-
+    
 # Scale data
 def scale_data(pitchers):
     num_data = pitchers[['Decisions', 'Wins_Over_Decisions',
@@ -129,7 +132,6 @@ def scale_data(pitchers):
     
     return pitchers
     
-pitchers = scale_data(pitchers)
 
 # Create dictionary of pitchers
 def create_dictionary(pitchers):
@@ -148,70 +150,61 @@ def create_dictionary(pitchers):
     
     return player_dictionary
 
-player_dictionary = create_dictionary(pitchers)
 
 # Euclidean Distance Function
-def sim_distance(prefs, p1, p2):
-    # Get the list of shared_items
+def sim_distance(atts, p1, p2):
     si = {}
-    for item in prefs[p1]:
-        if item in prefs[p2]:
+    for item in atts[p1]:
+        if item in atts[p2]:
             si[item] = 1
-    # If they have no ratings in common, return 0
+            
     if len(si) == 0:
         return 0
-    # Add up the squares of all the differences
-    sum_of_squares = sum([pow(prefs[p1][item] - prefs[p2][item], 2) for item in
-                         prefs[p1] if item in prefs[p2]])
+
+    sum_of_squares = sum([pow(atts[p1][item] - atts[p2][item], 2) for item in
+                         atts[p1] if item in atts[p2]])
                          
     return 1 / (1 + sqrt(sum_of_squares))
 
-# Top matches
-def topMatches(prefs, person, n=15, similarity=sim_distance):
+  
+# Get top matches
+def top_matches(atts, person, n=15, similarity=sim_distance):
 
-    scores = [(similarity(prefs, person, other), other) for other in prefs
+    scores = [(similarity(atts, person, other), other) for other in atts
               if other != person]
     scores.sort()
     scores.reverse()
     return scores[0:n]
 
-# Run
+  
+# Run the similarity analysis
 def get_top_matches(player_and_year):
-    df = topMatches(player_dictionary, player_and_year)
+    df = top_matches(player_dictionary, player_and_year)
     df = pd.DataFrame(df)
     df.columns = ['Similarity', 'Pitcher_and_Year']
     return df
-    
-pedro2000 = get_top_matches('Pedro Martinez 2000')
-clemens1997 = get_top_matches('Roger Clemens 1997')
-johnson2002 = get_top_matches('Randy Johnson 2002')
-greinke2009 = get_top_matches('Zack Greinke 2009')
+  
+if __name__ == "__main__:
+    pitchers = ingest_data()
+    pitchers = clean_data(pitchers)
+    pitchers = scale_data(pitchers)
+    player_dictionary = create_dictionary(pitchers)
+  
+    pedro2000 = get_top_matches('Pedro Martinez 2000')
+    clemens1997 = get_top_matches('Roger Clemens 1997')
+    johnson2002 = get_top_matches('Randy Johnson 2002')
+    greinke2009 = get_top_matches('Zack Greinke 2009')
+    maddux1992 = get_top_matches('Greg Maddux 1992')
+    schilling2001 = get_top_matches('Curt Schilling 2001')
+    rivera2004 = get_top_matches('Mariano Rivera 2004')
+    gagne2003 = get_top_matches('Eric Gagne 2003')
 
-maddux1992 = get_top_matches('Greg Maddux 1992')
-schilling2001 = get_top_matches('Curt Schilling 2001')
-rivera2004 = get_top_matches('Mariano Rivera 2004')
-gagne2003 = get_top_matches('Eric Gagne 2003')
+    pedro2000.to_csv('pedro2000.csv', index=False)
+    clemens1997.to_csv('clemens1997.csv', index=False)
+    johnson2002.to_csv('johnson2002.csv', index=False)
+    greinke2009.to_csv('greinke2009.csv', index=False)
+    maddux1992.to_csv('maddux1992.csv', index=False)
+    schilling2001.to_csv('schilling2001.csv', index=False)
+    rivera2004.to_csv('rivera2004.csv', index=False)
+    gagne2003.to_csv('gagne2003.csv', index=False)
 
-# Write results to CSV for visualization
-pedro2000.to_csv('pedro2000.csv', index=False)
-clemens1997.to_csv('clemens1997.csv', index=False)
-johnson2002.to_csv('johnson2002.csv', index=False)
-greinke2009.to_csv('greinke2009.csv', index=False)
-maddux1992.to_csv('maddux1992.csv', index=False)
-schilling2001.to_csv('schilling2001.csv', index=False)
-rivera2004.to_csv('rivera2004.csv', index=False)
-gagne2003.to_csv('gagne2003.csv', index=False)
-
-# Subset Pedro
-pedro_list = pedro2000['Pitcher_and_Year'].tolist()
-pedro_sims = pitchers.loc[pitchers['Player_and_Year'].isin (pedro_list)]
-pedro_himself = pitchers.loc[pitchers['Player_and_Year'] == 'Pedro Martinez 2000']
-pedro_sims = pd.concat([pedro_sims, pedro_himself], axis=0)
-pedro_sims.to_csv('pedro_sims.csv', index=False)
-
-# Subset Greinke
-greinke_list = greinke2009['Pitcher_and_Year'].tolist()
-greinke_sims = pitchers.loc[pitchers['Player_and_Year'].isin (greinke_list)]
-greinke_himself = pitchers.loc[pitchers['Player_and_Year'] == 'Zack Greinke 2009']
-greinke_sims = pd.concat([greinke_sims, greinke_himself], axis=0)
-greinke_sims.to_csv('greinke_sims.csv', index=False)
